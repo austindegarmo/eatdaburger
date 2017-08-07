203293756804d89af03a0dff3aad3115512ca902
Austin DeGarmo <austindegarmo@yahoo.com>
2017-08-06 21:58:02 -0700

    puh

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,11 +5,42 @@ var router = express.Router();
 var burger = require(".../models/burger.js");
 
 router.get("/", function(req, res) {
-	cat.all(function(data) {
+	burger.all(function(data) {
 		var burObject = {
 			burger: data
 		};
 		console.log(burObject);
-		res.render("index", burObject)
+		res.render("index", burObject);
 	});
 });
+
+router.post("/", function(req, res) {
+  burger.create([
+    "name", "eaten"
+  ], [
+    req.body.name, req.body.eaten
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.update({
+    eaten: req.body.eaten
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+router.delete("/:burger_name", function(req, res){
+	var condition = "burger_name = " + req.params.burger_name;
+
+	burger.delete(condition, function() {
+		res.redirect("/");
+	})
+
+})
